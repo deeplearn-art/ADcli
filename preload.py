1727f14057c1a08f4747cbf4c69258238bfa02a4
Johannezz <johannezz.music@gmail.com>
2023-12-01 06:42:21 +0200

    fix last line

--- preload.py
+++ preload.py
@@ -7,4 +7,5 @@ from pathlib import Path
 model.get_base_model( "runwayml/stable-diffusion-v1-5",local_dir=Path("/src/data/models/huggingface"))
 CLIPModel.from_pretrained("openai/clip-vit-large-patch14", cache_dir="/src/.cache",local_files_only=False)
 CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14", cache_dir="/src/.cache", local_files_only=False)
-ControlNetModel.from_pretrained('lllyasviel/control_v11p_sd15_openpose',cache_dir='/src/.cache/huggingface/hub', torch_>OpenposeDetector.from_pretrained('lllyasviel/Annotators',cache_dir='/src/.cache/huggingface/hub')
+ControlNetModel.from_pretrained('lllyasviel/control_v11p_sd15_openpose',cache_dir='/src/.cache/huggingface/hub', torch_dtype=torch.float16)
+OpenposeDetector.from_pretrained('lllyasviel/Annotators',cache_dir='/src/.cache/huggingface/hub')
