26f764144d6fb8630fa65a909cae13ca134a02f7
Johannezz <johannezz.music@gmail.com>
2023-12-13 22:14:27 +0200

    new models + ipadapter + pillow-simd + v1-inference.yaml

--- preload.py
+++ preload.py
@@ -3,9 +3,11 @@ import torch
 from controlnet_aux import OpenposeDetector
 from diffusers import ControlNetModel
 from animatediff.utils import model
+from animatediff.utils.util import prepare_ip_adapter
 from pathlib import Path
 model.get_base_model( "runwayml/stable-diffusion-v1-5",local_dir=Path("/src/data/models/huggingface"))
 CLIPModel.from_pretrained("openai/clip-vit-large-patch14", cache_dir="/src/.cache",local_files_only=False)
 CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14", cache_dir="/src/.cache", local_files_only=False)
 ControlNetModel.from_pretrained('lllyasviel/control_v11p_sd15_openpose',cache_dir='/src/.cache/huggingface/hub', torch_dtype=torch.float16)
 OpenposeDetector.from_pretrained('lllyasviel/Annotators',cache_dir='/src/.cache/huggingface/hub')
+#prepare_ip_adapter()
